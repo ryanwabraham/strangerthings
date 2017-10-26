df071fb96d70380f2cd7b9b3b20579f694c2da29
Ryan Abraham <rabraham@uaudio.com>
2017-10-26 15:55:22 -0700

    Remove easing parameter from create and move letter functions

--- js/animation.js
+++ js/animation.js
@@ -13,47 +13,47 @@ start.addEventListener("click", function() {
 });
 
 function playAnimation() {
-	moveLetter('start', 0, 0, 1, 1, 1, 0, 'easeOutQuad', 2000);
+	moveLetter('start', 0, 0, 1, 1, 1, 0, 2000);
 	music.play();
 	//A - 3000ms
-	setTimeout(function() {createLetter('A', '24.5%', '30%', '800rem', 0, 0, .75, 0, 1, 1, 'easeOutQuad', 8550, 5700)}, 2800);
+	setTimeout(function() {createLetter('A', '24.5%', '30%', '800rem', 0, 0, .75, 0, 1, 1, 8550, 5700)}, 2800);
 	// N - 8000ms
-	setTimeout(function() {createLetter('N', '-10%', 0, '185rem', 0, 300, .90, 0, 1, 1, 'easeOutQuad', 4000, 2500)}, 8500);
+	setTimeout(function() {createLetter('N', '-10%', 0, '185rem', 0, 300, .90, 0, 1, 1, 4000, 2500)}, 8500);
 	// RI - 11000ms
-	setTimeout(function() {createLetter('R', '-50%', '26%', '175rem', -200, 300, .95, 0, 1, 1, 'easeOutQuad', 4500, 3000)}, 11000);
-	setTimeout(function() {createLetter('I', '50%', '-20%', '175rem', 200, 300, .95, 0, 1, 1, 'easeOutQuad', 4500, 3000)}, 11000);
+	setTimeout(function() {createLetter('R', '-50%', '26%', '175rem', -200, 300, .95, 0, 1, 1, 4500, 3000)}, 11000);
+	setTimeout(function() {createLetter('I', '50%', '-20%', '175rem', 200, 300, .95, 0, 1, 1, 4500, 3000)}, 11000);
 	// SG - 14000ms
-	setTimeout(function() {createLetter('S', '10%', '50%', '120rem', -20, -30, .90, -15, 1, 1, 'easeOutQuad', 5500, 3000)}, 14000);
-	setTimeout(function() {createLetter('G', '10%', '-55%', '120rem', 5, -15, .90, -35, 1, 1, 'easeOutQuad', 5500, 3000)}, 14000);
+	setTimeout(function() {createLetter('S', '10%', '50%', '120rem', -20, -30, .90, -15, 1, 1, 5500, 3000)}, 14000);
+	setTimeout(function() {createLetter('G', '10%', '-55%', '120rem', 5, -15, .90, -35, 1, 1, 5500, 3000)}, 14000);
 	//SR - 17000ms
-	setTimeout(function() {createLetter('S', '20%', '40%', '150rem', -75, -150, .90, 0, 1, 1, 'easeOutQuad', 4200, 2800)}, 17000);
-	setTimeout(function() {createLetter('R', '-85%', '15%', '150rem', 100, -75, .90, 0, 1, 1, 'easeOutQuad', 4200, 2800)}, 17000);
+	setTimeout(function() {createLetter('S', '20%', '40%', '150rem', -75, -150, .90, 0, 1, 1, 4200, 2800)}, 17000);
+	setTimeout(function() {createLetter('R', '-85%', '15%', '150rem', 100, -75, .90, 0, 1, 1, 4200, 2800)}, 17000);
 	//TART - 19000ms
-	setTimeout(function() {createLetter('T', '-25%', '-55%', '100rem', -150, 80, 1, 0, 1, 0, 'easeOutQuad', 8000, 6000)}, 19800);
-	setTimeout(function() {createLetter('A', '60%', '-55%', '100rem', 150, 80, 1, 0, 1, 0, 'easeOutQuad', 8000, 6000)}, 19800);
-	setTimeout(function() {createLetter('R', '60%', '45%', '100rem', -150, 80, 1, 0, 1, 0, 'easeOutQuad', 8000, 6000)}, 19800);
-	setTimeout(function() {createLetter('T', '-40%', '45%', '100rem', 150, 80, 1, 0, 1, 0, 'easeOutQuad', 8000, 6000)}, 19800);
+	setTimeout(function() {createLetter('T', '-25%', '-55%', '100rem', -150, 80, 1, 0, 1, 0, 8000, 6000)}, 19800);
+	setTimeout(function() {createLetter('A', '60%', '-55%', '100rem', 150, 80, 1, 0, 1, 0, 8000, 6000)}, 19800);
+	setTimeout(function() {createLetter('R', '60%', '45%', '100rem', -150, 80, 1, 0, 1, 0, 8000, 6000)}, 19800);
+	setTimeout(function() {createLetter('T', '-40%', '45%', '100rem', 150, 80, 1, 0, 1, 0, 8000, 6000)}, 19800);
 	//NG - 21000ms
-	setTimeout(function() {createLetter('N', '-25%', 0, '30rem', 150, 0, 1, 0, 0, 1, 'easeOutQuad', 6750, 4500)}, 21000);
-	setTimeout(function() {createLetter('G', '25%', 0, '30rem', -150, 0, 1, 0, 0, 1, 'easeOutQuad', 6750, 4500)}, 21000);
+	setTimeout(function() {createLetter('N', '-25%', 0, '30rem', 150, 0, 1, 0, 0, 1, 6750, 4500)}, 21000);
+	setTimeout(function() {createLetter('G', '25%', 0, '30rem', -150, 0, 1, 0, 0, 1, 6750, 4500)}, 21000);
 	//All letters - 25500ms
 	setTimeout(function() {
 		logoWrapper.className = '';
-		moveLetter('logo-wrapper', 0, 0, 5, .5, 1, 1, 'easeOutQuad', 25500);
-		moveLetter('a', '-25%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('n', '25%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('g', '50%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('i', '-35%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('g2', '25%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('r', '-75%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('n2', 0, '100%', 1, 1, 1, 1, 'easeOutQuad', 8000);
-		moveLetter('t2', '-100%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('t', 0, '-150%', 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('h', 0, '200%', 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('s2', 0, '150%', 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('e', 0, '-125%', 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('s', '-300%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
-		moveLetter('r2', '300%', 0, 1, 1, 1, 1, 'easeOutQuad', 12000);
+		moveLetter('logo-wrapper', 0, 0, 5, .5, 1, 1, 25500);
+		moveLetter('a', '-25%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('n', '25%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('g', '50%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('i', '-35%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('g2', '25%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('r', '-75%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('n2', 0, '100%', 1, 1, 1, 1, 8000);
+		moveLetter('t2', '-100%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('t', 0, '-150%', 1, 1, 1, 1, 12000);
+		moveLetter('h', 0, '200%', 1, 1, 1, 1, 12000);
+		moveLetter('s2', 0, '150%', 1, 1, 1, 1, 12000);
+		moveLetter('e', 0, '-125%', 1, 1, 1, 1, 12000);
+		moveLetter('s', '-300%', 0, 1, 1, 1, 1, 12000);
+		moveLetter('r2', '300%', 0, 1, 1, 1, 1, 12000);
 	}, 25500);
 	//Show hrs - 39800ms
 	setTimeout(function() {
@@ -66,10 +66,10 @@ function playAnimation() {
 	}, 39800);
 	//Fade out - 45000
 	setTimeout(function() {
-		moveLetter('main-wrapper', 0, 0, 1, 1, 1, 0, 'easeOutQuad', 5000);
+		moveLetter('main-wrapper', 0, 0, 1, 1, 1, 0, 5000);
 	}, 45000);
 
-	function createLetter(letter, offsetX, offsetY, fontSize, translateX, translateY, scale, rotate, startingOpacity, opacity, easing, transitionDuration, deleteAfter) {
+	function createLetter(letter, offsetX, offsetY, fontSize, translateX, translateY, scale, rotate, startingOpacity, opacity, transitionDuration, deleteAfter) {
 		var el = document.createElement('h1');
 		el.innerHTML = letter;
 		el.style.fontSize = fontSize;
@@ -83,7 +83,7 @@ function playAnimation() {
 			scale: scale,
 			rotate: rotate,
 			opacity: [startingOpacity, opacity],
-			easing: easing,
+			easing: 'easeOutQuad',
 			duration: transitionDuration
 		});
 		document.body.appendChild(el);
@@ -93,7 +93,7 @@ function playAnimation() {
 		}, deleteAfter);
 	}
 
-	function moveLetter(id, offsetX, offsetY, scaleFrom, scaleTo, startingOpacity, opacity, easing, transitionDuration) {
+	function moveLetter(id, offsetX, offsetY, scaleFrom, scaleTo, startingOpacity, opacity, transitionDuration) {
 		var el = document.getElementById(id);
 
 		anime({
@@ -102,7 +102,7 @@ function playAnimation() {
 			translateY: [offsetY, 0],
 			scale: [scaleFrom, scaleTo],
 			opacity: [startingOpacity, opacity],
-			easing: easing,
+			easing: 'easeOutQuad',
 			duration: transitionDuration
 		});
 	}
